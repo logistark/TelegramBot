884cc5feaf76143214f04e2ad18cc21cbe623f5e
Jose <jimenezsaezjoseantonio@gmail.com>
2017-01-24 22:33:29 +0100

    Changed DTOs
    Changed dtos, in order to add default values.
    Added GetMe, GetUpdates, SendMessage DTOs

--- src/main/scala/ADTs/TelegramDTO.scala
+++ src/main/scala/ADTs/TelegramDTO.scala
@@ -1,19 +1,27 @@
 package ADTs
 
-import ADTs.TelegramDTO.ReplyMarkup.{InlineKeyboardMarkup, ReplyKeyboardMarkup}
-import shapeless.:+:
 
 object TelegramDTO {
   sealed trait ReplyMarkup
-  object ReplyMarkup{
+  object ReplyMarkups {
     case class InlineKeyboardMarkup() extends ReplyMarkup
     case class ReplyKeyboardMarkup() extends ReplyMarkup
     case class ReplyKeyboardHide() extends ReplyMarkup
     case class ForceReply() extends ReplyMarkup
   }
 
-  case class User(id: Int, first_name: String, last_name: Option[String], username: Option[String])
-  case class Chat(id: Int, _type: String, title: Option[String], username: Option[String], first_name: Option[String], last_name: Option[String])
+  final case class GetMe()
+  final case class GetUpdates(offset: Option[Int], limit: Option[Int], timeout: Option[Int])
+  final case class SendMessage(id: Int,
+                               text: String,
+                               parse_mode: Option[String],
+                               disable_web_page_preview: Option[Boolean],
+                               disable_notification: Option[Boolean],
+                               reply_to_message_id: Option[Int],
+                               reply_markup: ReplyMarkup )
+
+  case class User(id: Int, first_name: String, last_name: Option[String] = None, username: Option[String] = None)
+  case class Chat(id: Int, _type: String, title: Option[String] = None, username: Option[String] = None, first_name: Option[String] = None, last_name: Option[String] = None)
   case class Audio(file_id: String, duration: Int, performer: Option[String], title: Option[String], mime_type: Option[String], file_size: Option[Int])
   case class Document(file_id: String, thumb: Option[PhotoSize], file_name: Option[String], mime_type: Option[String], file_size: Option[Int])
   case class PhotoSize(file_id: String, width: Int, height: Int, file_size: Option[String])
@@ -26,32 +34,32 @@ object TelegramDTO {
   case class UserProfilePhotos(total_count: Int, photos: Seq[Seq[String]])
   case class File(file_id: String, file_size: Option[Int], file_path: Option[String])
   case class Message(message_id: Int,
-                   from: Option[User],
+                   from: Option[User] = None,
                    date: Int,
                    chat: Chat,
-                   forward_from: Option[User],
-                   forward_date: Option[Int],
-                   reply_to_message: Option[Message],
-                   text: Option[String],
-                   audio: Option[Audio],
-                   document: Option[Document],
-                   photo: Option[List[PhotoSize]],
-                   sticker: Option[Sticker],
-                   video: Option[Video],
-                   voice: Option[Voice],
-                   caption: Option[String],
-                   contact: Option[Contact],
-                   location: Option[Location],
-                   new_chat_participant: Option[User],
-                   left_chat_participant: Option[User],
-                   new_chat_title: Option[String],
-                   new_chat_photo: Option[List[PhotoSize]],
-                   delete_chat_photo: Option[Boolean],
-                   group_chat_created: Option[Boolean],
-                   supergroup_chat_created: Option[Boolean],
-                   channel_chat_created: Option[Boolean],
-                   migrate_to_chat_id: Option[Int],
-                   migrate_from_chat_id: Option[Int]
+                   forward_from: Option[User] = None,
+                   forward_date: Option[Int] = None,
+                   reply_to_message: Option[Message] = None,
+                   text: Option[String] = None,
+                   audio: Option[Audio] = None,
+                   document: Option[Document] = None,
+                   photo: Option[List[PhotoSize]] = None,
+                   sticker: Option[Sticker] = None,
+                   video: Option[Video] = None,
+                   voice: Option[Voice] = None,
+                   caption: Option[String] = None,
+                   contact: Option[Contact] = None,
+                   location: Option[Location] = None,
+                   new_chat_participant: Option[User] = None,
+                   left_chat_participant: Option[User] = None,
+                   new_chat_title: Option[String] = None,
+                   new_chat_photo: Option[List[PhotoSize]] = None,
+                   delete_chat_photo: Option[Boolean] = None,
+                   group_chat_created: Option[Boolean] = None,
+                   supergroup_chat_created: Option[Boolean] = None,
+                   channel_chat_created: Option[Boolean] = None,
+                   migrate_to_chat_id: Option[Int] = None,
+                   migrate_from_chat_id: Option[Int] = None
                   )
   case class MessageEntitity(_type: String, offset: Int, length: Int, url: Option[String])
   case class InlineQueryResult()
